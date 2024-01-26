e1d1f7d52dc3d138562e88d3237d07ecd300331c
mhadrich <hadrichmalek@gmail.com>
2024-01-26 17:00:52 +0100

    favicon/name and wip component

--- src/Components/WIP.js
+++ src/Components/WIP.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function WIP() {
   return (
-    <div className="flex z-50 items-center justify-center fixed bg-yellow-500 h-10 w-full uppercase font-black">
+    <div className="flex z-50 items-center justify-center fixed bg-yellow-500 h-10 w-full uppercase font-black text-sm sm:lg:text-base">
       This website is a work in progress!
     </div>
   );
